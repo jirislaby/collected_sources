9dbbf865cde03cd137ca99f099246432760cae1d
Jiri Slaby <jirislaby@gmail.com>
2015-02-26 13:27:52 +0100

    simpler form

--- read-x-bins/read.c
+++ read-x-bins/read.c
@@ -12,7 +12,7 @@ static void dump_file(unsigned long from, unsigned long to)
 {
 	printf("\n===\n%lx-%lx\n", from, to);
 
-	int fd = open("bin", O_WRONLY|O_CREAT|O_APPEND, 0755);
+	int fd = open("bin", O_WRONLY|O_CREAT, 0755);
 	if (fd < 0) {
 		warn("open w");
 		return;
@@ -23,7 +23,7 @@ static void dump_file(unsigned long from, unsigned long to)
 
 static void pr(int fd)
 {
-	char buf[10000], *p = buf;
+	char buf[10000];
 	ssize_t sz;
 	unsigned long from, to;
 
@@ -36,40 +36,18 @@ static void pr(int fd)
 	if (write(1, buf, sz) != sz)
 		warn("write");
 
-	if (sscanf(p, "%lx-%lx", &from, &to) != 2) {
+	if (sscanf(buf, "%lx-%lx", &from, &to) != 2) {
 		warn("sscanf");
 		return;
 	}
-	while (*p != '\n')
-		p++;
-	p++;
 
 	dump_file(from, to);
-
-	if (sscanf(p, "%lx-%lx", &from, &to) != 2) {
-		warn("sscanf");
-		return;
-	}
-	while (*p != '\n')
-		p++;
-	p++;
-//	dump_file(from, to);
-
-	if (sscanf(p, "%lx-%lx", &from, &to) != 2) {
-		warn("sscanf");
-		return;
-	}
-	while (*p != '\n')
-		p++;
-//	dump_file(from, to);
-
 }
 
-static void sig_h(int s)
+static void __attribute__ ((constructor)) read_init(void)
 {
 	int fd = open("/proc/self/maps", O_RDONLY);
 
-	printf("ahoj %p\n", pr);
 	if (fd < 0) {
 		warn("open");
 		return;
@@ -80,15 +58,8 @@ static void sig_h(int s)
 	close(fd);
 }
 
-static void __attribute__ ((constructor)) read_init(void)
-{
-/*	signal(SIGALRM, sig_h);
-	alarm(1);*/
-	sig_h(SIGALRM);
-}
-
 int __attribute__ ((weak)) main()
 {
-	sig_h(SIGALRM);
+	read_init();
 	return 0;
 }
