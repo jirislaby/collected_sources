0ed1d5b3db9db3ae7e1f78ed581e11b4c548ecea
Jiri Slaby <jirislaby@gmail.com>
2016-03-19 10:05:28 +0100

    vhci: allow disabling write
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- vhci.c
+++ vhci.c
@@ -7,26 +7,33 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 
+#define DO_WRITE 0
+
 int main()
 {
+#if DO_WRITE
 	char buf[] = { 0xff, 0 };
 	struct iovec iov = {
 		.iov_base = buf,
 		.iov_len = sizeof(buf),
 	};
+#endif
 	int fd;
 
 	while (1) {
 		fd = open("/dev/vhci", O_RDWR);
 		if (fd < 0)
 			err(1, "open");
-
+#if DO_WRITE
 		usleep(50);
 
 		if (writev(fd, &iov, 1) < 0)
 			err(1, "writev");
 
 		usleep(50);
+#else
+		usleep(100);
+#endif
 
 		close(fd);
 	}
