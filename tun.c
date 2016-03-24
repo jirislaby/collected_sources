d1e798dd06e79d1f51343a59b11fa2a190cc79b5
Jiri Slaby <jirislaby@gmail.com>
2016-03-24 10:49:43 +0100

    tun: add stack-out-of-bounds reproducer
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

---
+++ tun.c
@@ -0,0 +1,36 @@
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <linux/if.h>
+#include <linux/if_tun.h>
+
+int main()
+{
+	struct ifreq ifreq = {
+		.ifr_ifrn.ifrn_name = "kill",
+		.ifr_ifru.ifru_flags = IFF_NO_PI | IFF_VNET_HDR | IFF_TUN,
+	};
+	char blank[10] = {};
+	int r0;
+
+	r0 = open("/dev/net/tun", O_RDWR);
+	if (r0 < 0)
+		err(1, "open");
+
+	if (ioctl(r0, TUNSETIFF, &ifreq) < 0)
+		err(1, "ioctl");
+
+	if (write(r0, blank, sizeof(blank)) < 0)
+		err(1, "write");
+
+	close(r0);
+
+	return 0;
+}
