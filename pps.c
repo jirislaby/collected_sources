a758d35f9416e5161f7953a381d1111d2b621081
Jiri Slaby <jirislaby@gmail.com>
2016-11-05 20:41:09 +0000

    pps: add fork
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- pps.c
+++ pps.c
@@ -1,13 +1,31 @@
+#include <unistd.h>
+
 #include <sys/fcntl.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <sys/wait.h>
 
 #include <linux/tty.h>
 
-int main()
+static void km()
 {
 	int r0 = open("/dev/ptmx", O_RDONLY);
 	int val = N_PPS;
 	ioctl(r0, TIOCSETD, &val); // TIOCSETD
+}
 
+#define P 40
+
+int main()
+{
+	int a;
+
+	while (1) {
+		for (a = 0; a < P; a++)
+			if (!fork())
+				km();
+
+		for (a = 0; a < P; a++)
+			wait(NULL);
+	}
 }
