765d32471552038cbd74ac113a5978b66c2cacae
Jiri Slaby <jirislaby@gmail.com>
2017-06-23 11:57:32 +0000

    tty-loop-read: add
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

---
+++ tty/tty-loop-read.c
@@ -0,0 +1,29 @@
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char **argv)
+{
+	size_t rd;
+	int fd;
+	char buf[64];
+
+	while (1) {
+		fd = open(argv[1], O_RDWR);
+		if (fd < 0) {
+//			warn("open");
+			continue;
+		}
+		rd = read(fd, buf, sizeof(buf));
+		if (rd > 0)
+			write(1, buf, rd);
+
+		close(fd);
+	}
+	return 0;
+}
