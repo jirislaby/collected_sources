5369dad0ec0078e2f0fa8d83442aa9fc165942df
Jiri Slaby <jirislaby@gmail.com>
2020-01-23 10:32:16 +0100

    pty: add some more

--- tty/pty.c
+++ tty/pty.c
@@ -12,12 +12,45 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include <termios.h>
+
+void send(int ptmx, int fd)
+{
+	char buf[4096];
+	int counter = 0;
+
+	while (1) {
+		int op = rand() % 4;
+		if (ioctl(fd, TCXONC, op) < 0)
+			err(1, "strcmp");
+		usleep(100 * (1 + rand() % 20));
+		if (!(counter++ % 1000)) {
+			size_t rd = read(ptmx, buf, sizeof(buf));
+			printf("%d: %5zd %2x op=%d\n", getpid(), rd, rd > 0 ? buf[0] : 0, op);
+			buf[0] = 0;
+		}
+	}
+}
+
+#define THREADS 10
+
+void fork_send(int ptmx, int fd)
+{
+	int a;
+	for (a = 0; a < THREADS - 1; a++) {
+		if (!fork())
+			break;
+	}
+	srand(getpid());
+	send(ptmx, fd);
+}
+
 int main(int argc, char **argv)
 {
 	char path[PATH_MAX];
 	int fd, ptmx, rnd;
 	while (1) {
-		ptmx = open("/dev/ptmx", O_RDWR);
+		ptmx = open("/dev/ptmx", O_RDWR | O_NONBLOCK);
 		if (ptmx < 0)
 			err(1, "open ptmx");
 
@@ -40,6 +73,8 @@ int main(int argc, char **argv)
 			err(1, "read");
 		if (strncmp("ahoj", path, 4))
 			errx(1, "strcmp");
+
+		fork_send(ptmx, fd);
 
 		rnd = rand();
 		usleep(150 * (1 + rnd % 80));
