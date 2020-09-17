a9a37d70bcafd3a70fcd03938bd2eb7d205e87e2
Jiri Slaby <jirislaby@gmail.com>
2020-09-17 09:57:21 +0200

    kdgkb: improve

--- tty/kdgkb.c
+++ tty/kdgkb.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -13,7 +14,30 @@
 #include <linux/kd.h>
 #include <linux/keyboard.h>
 
+static char *func[MAX_NR_FUNC];
+
 static void get(int fd)
+{
+	unsigned i, j;
+
+	for (i = 0; i < MAX_NR_FUNC; i++) {
+		struct kbsentry kbs = {
+			.kb_func = i,
+			.kb_string = "X",
+		};
+		if (ioctl(fd, KDGKBSENT, &kbs) < 0)
+			err(1, "ioctl(KDGKBSENT) [%u]", i);
+		func[i] = strdup((char *)kbs.kb_string);
+		printf("[%u] ", i);
+		for (j = 0; j < strlen((char *)kbs.kb_string); j++) {
+			const char ch = kbs.kb_string[j];
+			putchar(isprint(ch) ? ch : '.');
+		}
+		puts("");
+	}
+}
+
+static void set(int fd)
 {
 	unsigned i;
 
@@ -21,25 +45,38 @@ static void get(int fd)
 		struct kbsentry kbs = {
 			.kb_func = i,
 		};
-		if (ioctl(fd, KDGKBSENT, &kbs) < 0)
-			err(1, "ioctl(KDGKBSENT)");
-		printf("[%u] %s\n", i, kbs.kb_string);
-	}
-}
+		if (!strlen(func[i]))
+			continue;
+		strcpy((char *)kbs.kb_string, func[i]);
 
-static void set(int fd)
-{
-	ioctl(fd, KDSKBSENT);
+		if (ioctl(fd, KDSKBSENT, &kbs) < 0)
+			err(1, "ioctl(KDSKBSENT) [%u]", i);
+	}
+
+	for (i = 0; i < MAX_NR_FUNC; i++) {
+		struct kbsentry kbs = {
+			.kb_func = i,
+		};
+		char *from = func[i];
+		if (!strlen(from))
+			from = "AHOJ";
+		strcpy((char *)kbs.kb_string, from);
+		strcat((char *)kbs.kb_string, from);
+
+		if (ioctl(fd, KDSKBSENT, &kbs) < 0)
+			err(1, "ioctl(KDSKBSENT) [%u]", i);
+	}
 }
 
 int main(int argc, char **argv)
 {
-	int fd = open(argv[1], O_RDWR);
+	int fd = open(argv[1] ? : "/dev/tty", O_RDWR);
 	if (fd < 0)
 		err(1, "open");
 
 	get(fd);
 	set(fd);
+	get(fd);
 
 	close(fd);
 	return 0;
