ae8109420d88f473c098ea4d5a3710a924eef17a
Jiri Slaby <jirislaby@gmail.com>
2021-01-26 11:07:19 +0100

    gnuefi: add example

---
+++ efi/main.c
@@ -0,0 +1,48 @@
+#include <efi.h>
+#include <efilib.h>
+
+static EFI_STATUS get_keystroke(EFI_INPUT_KEY *key)
+{
+	SIMPLE_INPUT_INTERFACE *ci = ST->ConIn;
+	UINTN EventIndex;
+	EFI_STATUS efi_status;
+
+	do {
+		gBS->WaitForEvent(1, &ci->WaitForKey, &EventIndex);
+		efi_status = ci->ReadKeyStroke(ci, key);
+	} while (efi_status == EFI_NOT_READY);
+
+	return efi_status;
+}
+
+static void msleep(unsigned int ms)
+{
+	gBS->Stall(1000 * (UINTN)ms);
+}
+
+EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
+{
+	CHAR16 buf[64];
+	EFI_INPUT_KEY key;
+
+	InitializeLib(image, systab);
+
+	gBS->SetWatchdogTimer(0, 0x0, 0, NULL);
+
+	systab->ConOut->Reset(systab->ConOut, FALSE);
+	systab->ConOut->EnableCursor(systab->ConOut, TRUE);
+
+	Print(L"Hello, world!\n");
+	Input(L"Input something: ", buf, sizeof(buf));
+	Print(L"\nYou wrote: %s\n", buf);
+
+	do {
+		Print(L"Press a key (ESC to exit)...\n");
+		get_keystroke(&key);
+		Print(L"\nYou pressed=%d/%d\n", key.ScanCode, key.UnicodeChar);
+	} while (key.ScanCode != 23);
+
+	msleep(3000);
+
+	return EFI_SUCCESS;
+}
