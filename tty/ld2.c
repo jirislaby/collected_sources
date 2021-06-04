06958e5e19f3477583219400d992195b5dc9f798
Jiri Slaby <jirislaby@gmail.com>
2021-06-04 09:07:27 +0200

    ld2: add rand_sched

--- tty/ld2.c
+++ tty/ld2.c
@@ -8,6 +8,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#include "../util.h"
+
 int main(int argc, char *argv[])
 {
 	int fd, ld1, ld2;
@@ -24,8 +26,10 @@ int main(int argc, char *argv[])
 	while (1) {
 		if (ioctl(fd, TIOCSETD, &ld1))
 			warn("ioctl");
+		rand_sched();
 		if (ioctl(fd, TIOCSETD, &ld2))
 			warn("ioctl");
+		rand_sched();
 	}
 	close(fd);
 }
