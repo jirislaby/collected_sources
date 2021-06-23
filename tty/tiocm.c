50ab920c994a351b8bc20a8384548e284177fb1b
Jiri Slaby <jirislaby@kernel.org>
2021-06-23 13:06:19 +0200

    tiocm: getopt to make it more configurable

--- tty/tiocm.c
+++ tty/tiocm.c
@@ -1,5 +1,6 @@
 #include <err.h>
 #include <fcntl.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,20 +10,24 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#define MKNAM(a) { #a, TIOCM_ ## a }
+#define TIO_BITS(op)	\
+	op(LE),		\
+	op(DTR),	\
+	op(RTS),	\
+	op(ST),		\
+	op(SR),		\
+	op(CTS),	\
+	op(CAR),	\
+	op(RNG),	\
+	op(DSR)
+
 static struct {
 	const char *name;
 	unsigned int bit;
 } names[] = {
-	MKNAM(LE),
-	MKNAM(DTR),
-	MKNAM(RTS),
-	MKNAM(ST),
-	MKNAM(SR),
-	MKNAM(CTS),
-	MKNAM(CAR),
-	MKNAM(RNG),
-	MKNAM(DSR),
+#define MKNAM(a) { #a, TIOCM_ ## a }
+	TIO_BITS(MKNAM)
+#undef MKNAM
 };
 
 static void dump_tiocm(int tio)
@@ -35,27 +40,57 @@ static void dump_tiocm(int tio)
 			printf("  %s\n", names[a].name);
 }
 
-static int get_tio_bit(const char *bit)
+static void usage(const char *program)
 {
-	int a;
-
-	for (a = 0; a < sizeof(names)/sizeof(*names); a++)
-		if (!strcmp(names[a].name, bit))
-			return names[a].bit;
-	return 0;
+	errx(100, "%s /dev/ttyS0", program);
 }
 
 int main(int argc, char **argv)
 {
+	static int opt_tiocm;
+	static const struct option opts[] = {
+#define MKOPT(a) { #a "+", 0, &opt_tiocm, TIOCM_ ## a }
+		TIO_BITS(MKOPT),
+#undef MKOPT
+#define MKOPT(a) { #a "-", 0, &opt_tiocm, TIOCM_ ## a }
+		TIO_BITS(MKOPT),
+#undef MKOPT
+#define MKOPT(a) { #a "*", 0, &opt_tiocm, TIOCM_ ## a }
+		TIO_BITS(MKOPT),
+#undef MKOPT
+		{ "sleep", 1, NULL, 's' },
+		{}
+	};
 	const char *tty;
-	int fd, tio, orig_tio;
+	int fd, tio, orig_tio, o, o_idx;
+	int clear = 0, set = 0, alter = 0;
+	unsigned int sleep = 0;
 
-	if (argc < 2)
-		errx(100, "%s /dev/ttyS0", argv[0]);
+	while ((o = getopt_long(argc, argv, "s:", opts, &o_idx)) >= 0) {
+		switch (o) {
+		case 0:
+			const char *name = opts[o_idx].name;
+			const char last = name[strlen(name) - 1];
 
-	tty = argv[1];
-	argv += 2;
-	argc -= 2;
+			if (last == '-')
+				clear |= opts[o_idx].val;
+			else if (last == '+')
+				set |= opts[o_idx].val;
+			else
+				alter |= opts[o_idx].val;
+			break;
+		case 's':
+			sleep = atoi(optarg);
+			break;
+		default:
+			usage(argv[0]);
+		}
+	}
+
+	if (argc - optind < 1)
+		usage(argv[0]);
+
+	tty = argv[optind];
 
 	fd = open(tty, O_RDWR);
 	if (fd < 0)
@@ -69,39 +104,30 @@ int main(int argc, char **argv)
 
 	dump_tiocm(tio);
 
-	while (argc--) {
-		const char *val = *argv;
-		int bit;
-		if (*val != '+' && *val != '-') {
-			warnx("ignoring '%s'", val);
-			continue;
-		}
+	tio |= set;
+	tio &= ~clear;
 
-		bit = get_tio_bit(val + 1);
-		if (!bit) {
-			warnx("ignoring '%s'", val);
-			continue;
-		}
+	if (tio != orig_tio || alter) {
+		do {
+			puts("Changing to:");
+			dump_tiocm(tio);
 
-		if (*val == '+')
-			tio |= bit;
-		else
-			tio &= ~bit;
-		argv++;
-	}
+			if (ioctl(fd, TIOCMSET, &tio))
+				err(2, "ioctl(TIOCMSET)");
 
-	if (tio != orig_tio) {
-		puts("Changing to:");
-		dump_tiocm(tio);
+			puts("Changed to:");
+			if (ioctl(fd, TIOCMGET, &tio))
+				err(2, "ioctl(TIOCMGET)");
 
-		if (ioctl(fd, TIOCMSET, &tio))
-			err(2, "ioctl(TIOCMSET)");
+			dump_tiocm(tio);
 
-		puts("Changed to:");
-		if (ioctl(fd, TIOCMGET, &tio))
-			err(2, "ioctl(TIOCMGET)");
+			if (sleep) {
+				printf("sleeping %u ms\n", sleep);
+				usleep(sleep * 1000);
+			}
 
-		dump_tiocm(tio);
+			tio ^= alter;
+		} while (alter);
 	}
 
 	close(fd);
