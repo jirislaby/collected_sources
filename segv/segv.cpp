1e353712f7e8c6f5a7857767dbdf91a7385412e4
Jiri Slaby <jirislaby@gmail.com>
2026-05-05 08:41:19 +0200

    segv: add

---
+++ segv/segv.cpp
@@ -0,0 +1,74 @@
+#define _POSIX_C_SOURCE
+
+#include <csignal>
+#include <iostream>
+#include <memory>
+#include <ucontext.h>
+
+#include <sys/ucontext.h>
+
+namespace {
+
+void dump_regs(const ucontext_t &ucontext)
+{
+	auto gregs = ucontext.uc_mcontext.gregs;
+	std::cerr << "=== dump registers ===\n";
+	std::cerr << "RIP=0x" << std::hex << gregs[REG_RIP] << '\n';
+	std::cerr << "RSP=0x" << std::hex << gregs[REG_RSP] << '\n';
+	std::cerr << "ERR=0x" << std::hex << gregs[REG_ERR] << '\n';
+	std::cerr << "TRAPNO=0x" << std::hex << gregs[REG_TRAPNO] << '\n';
+	std::cerr << "CR2=0x" << std::hex << gregs[REG_CR2] << '\n';
+	std::cerr << "=== end dump ===\n";
+}
+
+void segv(int, siginfo_t *info, void *_ucontext)
+{
+	std::cerr << "======================================\n";
+	std::cerr << "Segmentation fault (SIGSEGV) detected!\n";
+	int a{};
+	std::cerr << "stack=" << &a << '\n';
+	std::cerr << "code=" << info->si_code << '\n';
+	std::cerr << "addr=" << info->si_addr << '\n';
+	std::cerr << "errno=" << info->si_errno << '\n';
+
+	dump_regs(*static_cast<ucontext_t *>(_ucontext));
+	std::exit(1);
+}
+
+[[gnu::noinline]] void die(int *x)
+{
+	*x = 42;
+}
+}
+
+int main(int argc, char **)
+{
+	bool altstack = argc > 1;
+	struct sigaction sa{};
+	sa.sa_sigaction = segv;
+	sa.sa_flags = SA_SIGINFO | SA_ONSTACK;
+	auto stack = std::make_unique<char[]>(SIGSTKSZ);
+	stack_t newss{};
+	newss.ss_sp = stack.get();
+	newss.ss_size = SIGSTKSZ;
+	if (altstack) {
+		std::cerr << "Using alternate signal stack at " << static_cast<void *>(stack.get()) << '\n';
+		::sigaltstack(&newss, nullptr);
+	} else {
+		std::cerr << "Not using alternate signal stack\n";
+	}
+	::sigaction(SIGSEGV, &sa, nullptr);
+	int *x = nullptr;
+	--x;
+	std::cerr << "stack=" << &x << '\n';
+	std::cerr << "x=" << x << '\n';
+	std::cerr << "die=" << reinterpret_cast<void *>(die) << '\n';
+	ucontext_t ucontext{};
+	::getcontext(&ucontext);
+	dump_regs(ucontext);
+
+	die(x);
+
+	return 0;
+}
+
